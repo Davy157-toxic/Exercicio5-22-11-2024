e53e4282c94d5b3c038473a90b56de81f578a62f
Davy <machadodavi3778@gmail.com>
2024-11-22 12:43:50 +0000

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+    public class IAMGAY
+    {
+       public static void Main(string[] args)
+        {
+        int X = 3;
+        
+         switch(X)
+         {
+            case 1:
+                Console.WriteLine("Domingo");
+             break;
+
+            case 2:
+                Console.WriteLine("Segunda-feira");
+             break;
+
+            case 3:
+                Console.WriteLine("Terça-feira");
+            break;
+
+            case 4:
+                Console.WriteLine("Quarta-feira");
+            break;
+
+            case 5:
+                Console.WriteLine("Quinta-feira");
+            break;
+
+            case 6:
+                Console.WriteLine("Sexta-feira");
+            break;
+               
+            case 7:
+                Console.WriteLine("Sabado");
+             break;
+
+             default:
+               Console.WriteLine("Numero invalido, introduza um numero de 1 a 7"
+              break ;
+         }
+       }
+    }
+
